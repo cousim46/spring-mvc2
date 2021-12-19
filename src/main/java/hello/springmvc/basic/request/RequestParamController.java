1a5f0b741497776447c06f5a9451408816daff8f
cousim46 <wjdghldns24@naver.com>
2021-12-19 18:44:15 +0900

    @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -84,7 +86,29 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap){
 
-        log.info("username={}, age={}",paramMap.get("username"),paramMap.get("age"));
+        log.info("username={}, age={}, abc={}",paramMap.get("username"),paramMap.get("age"),paramMap.get("abc"));
         return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+
+
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+        log.info("helloData={}",helloData);
+        return "ok";
+
+    }
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2( HelloData helloData) {
+
+
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+        log.info("helloData={}",helloData);
+        return "ok";
+
     }
 }
